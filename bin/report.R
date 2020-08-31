4d4bc1e560f613e7339d3a2f0218dc7dbb8ff543
Philipp Resl <resl.philipp@gmail.com>
2020-08-31 16:35:08 +0200

    Add ability to also change height of plots for report

--- bin/report.R
+++ bin/report.R
@@ -5,10 +5,11 @@ args <- commandArgs(trailingOnly=TRUE)
 
 busco_set <- args[1]
 mywidth <- as.numeric(args[2])
-table_file <- args[3]
-summary_file <- args[4]
-outfile1 <- args[5]
-outfile2 <- args[6]
+myheight <- as.numeric(args[3])
+table_file <- args[4]
+summary_file <- args[5]
+outfile1 <- args[6]
+outfile2 <- args[7]
 
 
 data2 <- read.csv(summary_file, sep="\t", header=T, stringsAsFactors = F, row.names = 1)
@@ -21,10 +22,10 @@ data$percent_complete <- NULL
 
 mdata <- melt(as.matrix(data))
 base_size <- 7
-p <- ggplot(mdata, aes(Var2, Var1, fill=value)) +geom_tile() + scale_fill_gradient(low="white", high="orange") +theme(axis.ticks = element_blank(),legend.position="None",axis.text.y = element_text(size=base_size*0.7), axis.text.x = element_text(size = base_size *0.8,angle=-90, hjust = 0,vjust=0.5, colour = "grey50"))+  xlab("Species") + ylab("BUSCO gene") + ggtitle(paste("Overview of complete single-copy BUSCOs per species (total genes=", as.character(total),") BUSCO set: ",busco_set, sep=""))
+p <- ggplot(mdata, aes(Var2, Var1, fill=value)) +geom_tile() + scale_fill_gradient(low="white", high="orange") +theme(axis.ticks = element_blank(),legend.position="None",axis.text.y = element_text(size=base_size*0.7), axis.text.x = element_text(size = base_size *0.8,angle=-90, hjust = 0,vjust=0.5, colour = "grey50"))+  xlab("BUSCO gene") + ylab("Species") + ggtitle(paste("Overview of complete single-copy BUSCOs per species (total genes=", as.character(total),") BUSCO set: ",busco_set, sep=""))
 
 
-pdf(file=outfile1, width=mywidth)
+pdf(file=outfile1, width=mywidth, height=myheight)
 print(p)
 dev.off()
 
@@ -36,7 +37,7 @@ base_size <- 12
 colnames(mdata2) <- c("species", "BUSCO category", "no. of genes")
 p2 <- ggplot(mdata2, aes_string(fill="`BUSCO category`", y="`no. of genes`", x="`species`")) +geom_bar(position="stack", stat="identity")+theme(axis.ticks.x = element_blank(), panel.grid=element_blank(), panel.background = element_blank(), axis.text.x = element_text(size = base_size *0.8,angle=-45, hjust = 0,vjust=1, colour = "grey50"))+scale_fill_manual(values=c("#6198B8", "#E37332", "#C6E88B", "#BCB2CF"))+ggtitle(paste("Overview of BUSCO results (total genes=", as.character(total),") BUSCO set: ",busco_set, sep=""))
 
-pdf(file=outfile2, width=mywidth/5)
+pdf(file=outfile2, width=mywidth)
 print(p2)
 dev.off()
 
--- rules/report.smk
+++ rules/report.smk
@@ -11,11 +11,12 @@ rule report:
 		"docker://continuumio/miniconda3:4.7.10"
 	params:
 		width = config["report"]["width"],
+		height = config["report"]["height"],
 		busco_set = config["busco"]["set"]
 	shell:
 		"""
 		# First extract information on buscos:
 		echo "species\tcomplete\tsingle_copy\tduplicated\tfragmented\tmissing\ttotal" > results/report/busco_summary.txt
 		for file in $(ls results/busco/*/run_busco/short_summary_busco.txt);do  name=$(echo $file | sed 's#results/busco/##' | sed 's#/run_busco/short_summary_busco.txt##'); printf $name; cat $file | grep -P '\t\d' | awk -F "\t" '{{printf "\t"$2}}' | awk '{{print}}'; done >> results/report/busco_summary.txt
-		Rscript bin/report.R {params.busco_set} {params.width} {input.busco_table} results/report/busco_summary.txt {output.busco_table} {output.busco_summary}
+		Rscript bin/report.R {params.busco_set} {params.width} {params.height} {input.busco_table} results/report/busco_summary.txt {output.busco_table} {output.busco_summary}
 		"""
