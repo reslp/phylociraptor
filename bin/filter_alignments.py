21e95309494cd4a172d492c7312b93c6aba03ed2
Philipp Resl <resl.philipp@gmail.com>
2021-03-08 17:51:31 +0100

    Several updates to filter_alignments.py to prepare changes for own mode

--- bin/filter_alignments.py
+++ bin/filter_alignments.py
@@ -13,25 +13,43 @@ pars = argparse.ArgumentParser(prog="filter_alignments.py", description = """Thi
 pars.add_argument('--alignments', dest="align", required=True, help="alignment files")
 pars.add_argument('--outdir', dest="outdir", required=True, help="output directory")
 pars.add_argument('--per_sample', dest="perseq", action='store_true', help="if set, only samples with duplicated sequences will be removed. Else the whole alignment")
+pars.add_argument('--min-parsimony', dest="minpars", required=True, help="The minimum number of parsimony informative sites and alignment must have to be kept.")
+pars.add_argument('--statistics-file', dest="statfile", required=True, help="Statistics file which contains alignment information(incl. parsimony informative sites; this file needs to be created with concat)")
 args=pars.parse_args()
 
 algn_list = args.align.split(" ")
 
+stats_dict = {}
+with open(args.statfile, "r") as stats:
+	stats.readline()
+	for stat in stats:
+		stats_dict[stat.split("\t")[0]] = int(stat.split("\t")[5])	
 for al in algn_list:
+	if os.stat(al).st_size == 0:
+		print(os.path.basename(al), "\tEMPTY")
+		continue
+	if stats_dict[os.path.basename(al)] < int(args.minpars):
+		print(os.path.basename(al), "\tNOT_INFORMATIVE")
+		continue
+	
 	seqfile = open(al, "r")
 	sequences = []
 	for seq_record in SeqIO.parse(seqfile, "fasta"):
 		sequences.append(seq_record)
 	names_list = [seq.id for seq in sequences]
+	if len(names_list) < 3:
+		print(os.path.basename(al), "\tTOO_FEW_SEQUENCES")
+		continue
 	if len(names_list) == len(set(names_list)):
-		print("Create symlink: ", args.outdir+"/"+al.split("/")[-1])
+		print("Create symlink: ", args.outdir+"/"+al.split("/")[-1], file=sys.stderr)
 		os.symlink(al, args.outdir+"/"+al.split("/")[-1])
+		print(os.path.basename(al), "\tPASS")
 	else:
-		print("Warning: File %s contains duplicated sequence IDs!" % al)
+		print("Warning: File %s contains duplicated sequence IDs!" % al, file=sys.stderr)
 		if (args.perseq == True):	
-			print("Duplicated sequences will be removed and copy of file will be made.")
+			print("Duplicated sequences will be removed and copy of file will be made.", file=sys.stderr)
 			dups = [name for name in names_list if names_list.count(name)>1]
-			print("Warning: Will remove %d sequences" % len(dups))
+			print("Warning: Will remove %d sequences" % len(dups), file=sys.stderr)
 			dups = set(dups)
 			sequences = [sequence for sequence in sequences if sequence.id not in dups]
 			outfile = open(args.outdir+"/"+al.split("/")[-1], "w")
@@ -39,7 +57,9 @@ for al in algn_list:
 				print(">"+sequence.id, file=outfile)
 				print(sequence.seq, file=outfile)					
 			outfile.close()
+			print(os.path.basename(al), "\tREMOVE_DUPLICATED_SEQUENCES")
 		else:
-			print("Warning: %s file will be discarded" % al)	
+			print("Warning: %s file will be discarded" % al)
+			print(os.path.basename(al), "\tREMOVED")	
 
 
