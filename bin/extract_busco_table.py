5a898f2002d21fd113ccf9f4acbb02fa6e64d61c
Philipp Resl <resl.philipp@gmail.com>
2021-06-10 08:54:04 +0200

    Adjust extract_busco_table script to work with tared busco results

--- bin/extract_busco_table.py
+++ bin/extract_busco_table.py
@@ -9,7 +9,7 @@ import argparse
 if sys.version_info[0] < 3:
     raise Exception("Must be using Python 3")
 
-pars = argparse.ArgumentParser(prog="get_busco_table.py", description = """This script will get all busco3 hmms and look in the busco results all specified genomes for the presence of the files""", epilog = """written by Philipp Resl""")
+pars = argparse.ArgumentParser(prog="extract_busco_table.py", description = """This script will get all busco3 hmms and look in the busco results all specified genomes for the presence of the files""", epilog = """written by Philipp Resl""")
 pars.add_argument('--hmm', dest="hmm_dir", required=True, help="Directory of the BUSCO hmms.")
 pars.add_argument('--busco_results', dest="busco_results", required=True, help="Results directory containing all BUSCO runs.")
 pars.add_argument('-o', dest="out", required=True, help="BUSCO table output file.")
@@ -31,8 +31,16 @@ for species in genomes:
 	outstring = species
 	print("Extracting HMMs for", species, file=sys.stderr)
 	try:
-		buscos = os.listdir(args.busco_results + species + "/run_busco/single_copy_busco_sequences/")
+		busco_listing_file = open(args.busco_results + species + "/run_busco/single_copy_busco_sequences.txt", "r")
+		buscos = []
+		for line in busco_listing_file:
+			line = line.strip()
+			line = line.split(" ")[-1]
+			line = line.split("/")[-1]
+			if ".faa" in line: # only take aa files, this should be enough
+				buscos.append(line)
 		buscos = [busco.strip(".faa") for busco in buscos]
+		busco_listing_file.close()
 		for hmm in hmms:
 			if hmm in buscos:
 				outstring += "\t"
