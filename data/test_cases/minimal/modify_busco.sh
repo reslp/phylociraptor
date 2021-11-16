c38e83f0d4abd9734668f287a4c77fadb7c4bb31
Philipp Resl <resl.philipp@gmail.com>
2021-11-16 14:12:31 +0100

    test-cases: After more info to readme of minimal testcase, plus changes after successful runthrough

--- data/test_cases/minimal/modify_busco.sh
+++ data/test_cases/minimal/modify_busco.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+
+echo "This script modifies the downloaded busco set so that it only includes 10 genes for use with the minimal test_case."
+
 buscos="EOG092C5OAL EOG092C5OPO EOG092C5Q82 EOG092C5S4U EOG092C5T6H EOG092C5U93 EOG092C5UXM EOG092C5V3D EOG092C5Z2K EOG092C608K"
 
-buscotmp="busco_tmp"
+buscotmp="busco_tmp/fungi_odb9"
 
 rm -rf $buscotmp
 
-mkdir $buscotmp $buscotmp/info $buscotmp/hmms $buscotmp/prfl
+mkdir -p $buscotmp $buscotmp/info $buscotmp/hmms $buscotmp/prfl
 
-buscodir="busco_set"
+buscodir="results/orthology/busco/busco_set/fungi_odb9"
+
+if [[ ! -d $buscodir ]]; 
+then
+	echo "Directory $buscodir not found. Did you run phylociraptor setup and is this script in the phylociraptor base directory?"
+	exit 1
+fi
 
 echo "Copy hmms and prfl files"
 
@@ -49,3 +58,11 @@ cp $buscodir/ancestral_variants $buscotmp/
 echo "Modify dataset.cfg"
 
 cat $buscodir/dataset.cfg | sed -e 's/number_of_BUSCOs=.*/number_of_BUSCOs=10/' > $buscotmp/dataset.cfg
+
+echo "Replace busco directory with modified version"
+echo "A Backup of the original busco directory will be kept in results/orthology/busco/busco_set_backup"
+
+mv $buscodir results/orthology/busco/busco_set_backup
+mv $buscotmp $buscodir
+
+echo "done"
