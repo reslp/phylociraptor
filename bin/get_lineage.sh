7dc193de8c3ff7eeecaf06f5c6144b2705a57f61
Philipp Resl <resl.philipp@gmail.com>
2022-09-22 15:55:11 +0200

    Fix util get-lineage and take care of some edge cases

--- bin/get_lineage.sh
+++ bin/get_lineage.sh
@@ -2,14 +2,16 @@
 set -e
 
 echo "get_lineage.sh: Provided data file: $1"
-echo "get_lineage.sh: Trying to download: ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz to data/lineage_information"
-rm -rf data/lienage_information
-mkdir -p data/lineage_information
-#wget -q "ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz" -P data/lineage_information
-echo "get_lineage.sh: Download complete. Will unpack now."
-#tar xvfz data/lineage_information/taxdump.tar.gz -C data/lineage_information
-echo "get_lineage.sh: Unpacking complete. Will reformat data with taxonkit:"
-echo "get_lineage.sh: Reformatting taxon data..."
+if [[ $2 != "keep" ]]; then
+	echo "get_lineage.sh: Trying to download: ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz to data/lineage_information"
+	rm -rf data/lineage_information
+	mkdir -p data/lineage_information
+	wget -q "ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz" -P data/lineage_information
+	echo "get_lineage.sh: Download complete. Will unpack now."
+	tar xvfz data/lineage_information/taxdump.tar.gz -C data/lineage_information
+	echo "get_lineage.sh: Unpacking complete. Will reformat data with taxonkit:"
+	echo "get_lineage.sh: Reformatting taxon data..."
+fi
 # this below is not yet properly implemented. Not sure how to handle this in the future:
 #echo "get_lineage_information.sh: INFO: If unknown species taxa are named in the format Genus_sp the taxid of the genus will be used to increase the amount of available lineage information for unknown taxa."
 #tail -n +2 $1 | awk -F"," '{print $1}' | sed 's/_/ /g' | sed 's/sp$//g' | taxonkit name2taxid --data-dir data/lineage_information | taxonkit lineage --data-dir data/lineage_information --taxid-field 2 -R > data/lineage_information/lineage_information.csv
