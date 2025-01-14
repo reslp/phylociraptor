5b3974c7f888f7619a0d0f223022bade2afd3602
Philipp Resl <resl.philipp@gmail.com>
2025-01-14 21:19:03 +0100

    Remove searchbox

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ extensions = ['recommonmark']
 html_css_files = ["theme.css"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ['templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
